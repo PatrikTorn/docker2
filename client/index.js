aa935be99eaea84d6b613f866a8cac3d54e89298
Patrik Torn <patrik.torn@vincit.fi>
2020-10-02 23:40:05 +0300

    Finish exercice

--- client/index.js
+++ client/index.js
@@ -1,16 +1,17 @@
-const app = require('express')();
-const axios = require('axios');
-const PORT = 3000;
+const app = require("express")();
+const axios = require("axios");
+const PORT = 8001;
 const SERVER_PORT = 4000;
 
 app.listen(PORT, () => {
-    console.log("Client listening on port", PORT);
-})
+  console.log("Client listening on port", PORT);
+});
 
-app.get('/', async (req, res) => {
-    const serverResponse = await axios.post(`http://localhost:${SERVER_PORT}`);
-    const serverIp = serverResponse.data;
-    const myIp = req.connection.localAddress + ':' + req.connection.localPort
-    const greetingText = "Hello from " + serverIp + " to " + myIp;
-    res.send(greetingText);
+app.get("/", async (req, res) => {
+  const serverResponse = await axios.post(`http://localhost:${SERVER_PORT}`);
+  const serverIp = serverResponse.data;
+  const myIp = req.connection.localAddress + ":" + req.connection.localPort;
+  const greetingText = "Hello from " + serverIp + " to " + myIp;
+  console.log(greetingText);
+  res.send(greetingText);
 });
